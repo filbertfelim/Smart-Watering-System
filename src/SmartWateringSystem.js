bb782c43cebf575dfb637170ae9ee5ebd80282fd
jessicaaall <jessicalistiaraa@gmail.com>
2024-06-06 10:52:40 +0700

    moisture monitoring and history

--- src/SmartWateringSystem.js
+++ src/SmartWateringSystem.js
@@ -1,25 +1,29 @@
 import React, { useState, useEffect } from "react";
-import client, { publishMessage } from "./mqttService";
+import client from "./mqttService";
 import { database } from "./firebase";
-import { push, ref, set } from "firebase/database";
+import { push, ref, set, onValue } from "firebase/database";
+import Typography from "@mui/joy/Typography";
+import CircularProgress from "@mui/joy/CircularProgress";
+import { LineChart } from "@mui/x-charts/LineChart";
+import { Box } from '@mui/system';
 
 const MQTT_TOPIC_MOISTURE = "smart/watering/moisture";
-const MQTT_TOPIC_CONTROL = "smart/watering";
 const MQTT_TOPIC_RELAY = "smart/watering/relay";
 
 const SmartWateringSystem = () => {
-  const [moisture, setMoisture] = useState(null);
+  const [moisture, setMoisture] = useState(20);
   const [relayState, setRelayState] = useState("OFF");
   const [postData, setPostData] = useState(false);
 
   useEffect(() => {
     const handleMoistureData = (message) => {
-      setMoisture(message.toString());
+      setMoisture(Number(message.toString()));
     };
 
     const handleRelayData = (message) => {
       setRelayState(message.toString());
     };
+
     client.on("message", (topic, message) => {
       if (topic === MQTT_TOPIC_MOISTURE) {
         handleMoistureData(message);
@@ -27,6 +31,11 @@ const SmartWateringSystem = () => {
         handleRelayData(message);
       }
     });
+
+    return () => {
+      client.removeListener("message", handleMoistureData);
+      client.removeListener("message", handleRelayData);
+    };
   }, []);
 
   useEffect(() => {
@@ -36,18 +45,13 @@ const SmartWateringSystem = () => {
     }, 2700000); // Send data every 45 minutes
 
     return () => clearInterval(interval);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [postData]);
-
-  const handlePumpToggle = () => {
-    publishMessage(MQTT_TOPIC_CONTROL, "TOGGLE");
-  };
+  }, [postData, moisture]);
 
   const postMoistureToFirebase = (moisture) => {
     if (moisture === null) {
       return;
     }
-    const timestamp = new Date().toString();
+    const timestamp = new Date().toISOString();
 
     const dataRef = ref(database, "moistureData");
     const newDataRef = push(dataRef);
@@ -66,13 +70,188 @@ const SmartWateringSystem = () => {
       });
   };
 
+  useEffect(() => {
+    fetchMoistureHistory();
+  });
+
+  const fetchMoistureHistory = () => {
+    const dataRef = ref(database, "moistureData");
+    
+    onValue(dataRef, (snapshot) => {
+      console.log("Snapshot received:", snapshot);
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        const formattedData = Object.keys(data).map(key => ({
+          datetime: data[key].datetime,
+          level: data[key].level,
+        }));
+        console.log("Moisture history from Firebase:", formattedData);
+      } else {
+        console.log("No data available");
+      }
+    }, (error) => {
+      console.error("Error listening to Firebase:", error);
+    });
+  };
+
   return (
-    <div>
-      <h1>Smart Watering System</h1>
-      <p>Moisture Level: {moisture}%</p>
-      <p>Relay State: {relayState}</p>
-      <button onClick={handlePumpToggle}>Toggle Pump</button>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "#FFFFFF",
+        padding: "20px",
+      }}
+    >
+      <header
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          marginBottom: "50px",
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img src="logo.png" alt="WaterWise Logo" style={{ width: "28px", paddingRight: "10px" }} />
+          <Typography style={{ fontSize: "15px", fontWeight: "500" }}>WaterWise</Typography>
+        </div>
+        <div style={{ textAlign: "center" }}>
+          <Typography
+            style={{
+              fontSize: "18px",
+              fontWeight: "400",
+              letterSpacing: "0.05em",
+              marginBottom: "6px",
+              marginTop: "10px"
+            }}
+          >
+            Smart Watering System
+          </Typography>
+          <Typography
+            style={{
+              fontSize: "30px",
+              fontWeight: "700",
+              marginBottom: "-6px",
+            }}
+          >
+            Check and Monitor your Plants'
+          </Typography>
+          <Typography
+            style={{
+              fontSize: "30px",
+              fontWeight: "700",
+              color: "#00B5AD",
+            }}
+          >
+            Moisture Level Here
+          </Typography>
+        </div>
+      </header>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", lg: "row" },
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "35px",
+          marginBottom: "20px"
+        }}
+      >
+        <Box
+          sx={{
+            background: "#ffffff",
+            padding: "25px",
+            borderRadius: "10px",
+            boxShadow: "0 4px 10px rgba(0, 0, 0, 0.15)",
+            width: "100%",
+            maxWidth: {xs: "320px", sm: "500px"},
+            height: "410px",
+            textAlign: "center",
+          }}
+        >
+          <Typography style={{ fontSize: "22px", fontWeight: "700", marginBottom: "32px" }}>
+            <span style={{ color: "#00B5AD" }}>Current </span>Moisture Level
+          </Typography>
+          <CircularProgress
+            size="lg"
+            sx={{
+              "--CircularProgress-size": "190px",
+              "--CircularProgress-trackThickness": "20px",
+              "--CircularProgress-progressThickness": "20px",
+              "--CircularProgress-progressColor":
+                moisture < 20 ? "red" : moisture <= 30 ? "orange" : "#22B428",
+              marginBottom: "15px",
+            }}
+            determinate
+            value={moisture}
+          >
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+              <Typography style={{ fontSize: "34px", fontWeight: "700", color: moisture < 20 ? "red" : moisture <= 30 ? "orange" : "#22B428" }}>{moisture}%</Typography>
+              <Typography style={{ fontSize: "14px", color: "grey" }}>moisturized</Typography>
+            </div>
+          </CircularProgress>
+          <Typography style={{ fontSize: "20px", marginTop: "10px", color: "black" }}>
+            Water Pump Status : <span style={{ fontWeight: "700" }}>{relayState}</span>
+          </Typography>
+          <div style={{ display: "flex", justifyContent: "space-around", marginTop: "30px" }}>
+            <Typography style={{ color: "#828282", fontSize: "18px", fontWeight: "600" }}>Low</Typography>
+            <Typography style={{ color: "#828282", fontSize: "18px", fontWeight: "600" }}>Medium</Typography>
+            <Typography style={{ color: "#828282", fontSize: "18px", fontWeight: "600" }}>High</Typography>
+          </div>
+          <div style={{ display: "flex", justifyContent: "space-around", marginTop: "4px", marginBottom: "4px" }}>
+            <Typography style={{ color: "red", fontSize: "20px", fontWeight: "700" }}>&lt; 20%</Typography>
+            <Typography style={{ color: "orange", fontSize: "20px", fontWeight: "700" }}>20 - 30%</Typography>
+            <Typography style={{ color: "#22B428", fontSize: "20px", fontWeight: "700" }}>&gt; 30%</Typography>
+          </div>
+        </Box>
+
+        <Box
+          sx={{
+            background: "#ffffff",
+            padding: "25px",
+            borderRadius: "10px",
+            boxShadow: "0 4px 10px rgba(0, 0, 0, 0.15)",
+            width: "100%",
+            maxWidth: {xs: "320px", sm: "500px"},
+            height: "410px",
+          }}
+        >
+          <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", marginLeft: "10px", marginRight: "10px" }}>
+            <Typography style={{ fontSize: "22px", fontWeight: "700" }}>
+              <span style={{ color: "#00B5AD" }}>Weekly </span>History
+            </Typography>
+            <Typography style={{ fontSize: "16px", fontWeight: "400", color: "#828282" }}>
+              (1 June - 7 June)
+            </Typography>
+          </div>
+          <LineChart
+            xAxis={[
+              { data: [1,2,3,4,5,6,7] 
+              }
+
+            ]}
+            series={[
+              {
+                data: [1,2,3,4,5,6,7]
+              },
+            ]}
+            height={300}
+          />
+          <div style={{ marginLeft: "20px", marginTop: "10px" }}>
+              <Typography style={{ fontSize: "18px" }}>
+                <span style={{ color: "#828282", fontWeight: "500" }}>Highest Level : </span>
+                <span style={{ color: "#00B5AD", fontWeight: "700" }}>% </span>
+                <span style={{ fontWeight: "600" }}>on</span>
+              </Typography>
+              <Typography style={{ fontSize: "18px" }}>
+                <span style={{ color: "#828282", fontWeight: "500" }}>Lowest Level : </span>
+                <span style={{ color: "#00B5AD", fontWeight: "700" }}>% </span>
+                <span style={{ fontWeight: "600" }}>on</span>
+              </Typography>
+          </div>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
